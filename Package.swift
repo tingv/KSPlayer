c091f058de30c09ab6ee031d75a0e717b53be4e1
kintan <kingslay@icloud.com>
2024-10-27 20:14:32 +0800

    remove submodule

--- Package.swift
+++ Package.swift
@@ -81,6 +81,6 @@ if FileManager.default.fileExists(atPath: ffmpegKitPath + "/Package.swift") {
     ]
 } else {
     package.dependencies += [
-        .package(url: "git@github.com:TracyPlayer/FFmpegKit.git", from: "7.0.3"),
+        .package(url: "git@github.com:TracyPlayer/FFmpegKit.git", from: "7.1.0"),
     ]
 }
