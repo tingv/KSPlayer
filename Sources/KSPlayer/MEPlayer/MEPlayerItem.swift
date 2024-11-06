1195beeaab3b37037026d3e15faa93b792983492
kintan <kingslay@icloud.com>
2024-11-06 22:43:16 +0800

    优化 seek

--- Sources/KSPlayer/MEPlayer/MEPlayerItem.swift
+++ Sources/KSPlayer/MEPlayer/MEPlayerItem.swift
@@ -669,7 +669,9 @@ extension MEPlayerItem {
                     increase *= Int64(AV_TIME_BASE)
                     timeStamp = Int64(time.seconds) * Int64(AV_TIME_BASE) + increase
                 }
-                let seekMin = increase > 0 ? timeStamp - increase + 2 : Int64.min
+                // 有遇到一个mov的视频，如果指定min，那就无法进行seek。
+//                let seekMin = increase > 0 ? timeStamp - increase : Int64.min
+                let seekMin = Int64.min
                 let seekMax = increase < 0 ? timeStamp - increase - 2 : Int64.max
 //                allPlayerItemTracks.forEach { $0.seek(time: seekToTime) }
                 // can not seek to key frame
