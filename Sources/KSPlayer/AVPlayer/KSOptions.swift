ada9444e26edd5e0f4e7146db84f8393b5844c4e
kintan <kingslay@icloud.com>
2025-01-09 22:37:03 +0800

    解决杜比音频问题

--- Sources/KSPlayer/AVPlayer/KSOptions.swift
+++ Sources/KSPlayer/AVPlayer/KSOptions.swift
@@ -331,7 +331,7 @@ open class KSOptions {
 
     open func audioFrameMaxCount(fps: Float, channelCount: Int) -> UInt8 {
         let count = (Int(fps) * channelCount) >> 2
-        return UInt8(min(count, 2 ^ 6))
+        return UInt8(min(count, 64))
     }
 
     // MARK: subtile options
@@ -469,7 +469,7 @@ open class KSOptions {
     }
 
     open func videoFrameMaxCount(fps _: Float, naturalSize _: CGSize, isLive _: Bool) -> UInt8 {
-        2 ^ 2
+        4
     }
 
     /// customize dar
